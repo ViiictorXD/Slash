c0c133fab615654aff68e450dfc7f1728e203174
ViiictorXD <viictor.santssz@gmail.com>
2025-03-14 21:19:00 -0300

    change test module

--- src/test/java/io/github/viiictorxd/discord/slash/command/TestCommand.java
+++ src/test/java/io/github/viiictorxd/discord/slash/command/TestCommand.java
@@ -3,11 +3,13 @@ package io.github.viiictorxd.discord.slash.command;
 import io.github.viiictorxd.discord.slash.SlashContext;
 import io.github.viiictorxd.discord.slash.annotation.Sender;
 import io.github.viiictorxd.discord.slash.annotation.Slash;
+import net.dv8tion.jda.api.Permission;
 
 public class TestCommand {
 
   @Slash(
-   name = "test"
+   name = "test",
+   permission = {Permission.ADMINISTRATOR, Permission.MESSAGE_SEND}
   )
   public void handleTest(@Sender SlashContext context) {
     context.getEvent().reply("Hello World!").queue();
